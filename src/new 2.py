31d5e1ecae7fcb074d6553be9498031615ed814d
hailer24 <40500174+hailer24@users.noreply.github.com>
2021-06-06 22:19:03 +0530

    Delete new 2.py

--- src/new 2.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import cv2
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-img = cv2.imread('Back.png')
-#plt.imread(img)
-imgplt = plt.imshow(img)
-plt.show()
